be978009e51cc3fd73c9c0ca0c9c3dd01e600c37
King <vaghasiyakunj42@gmail.com>
2026-07-21 20:56:44 +0530

    Time: 0 ms (100%), Space: 19.2 MB (94.57%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -9,21 +9,12 @@ class Solution:
         for i in range(9):
             for j in range(9):
                 val = board[i][j]
-                
-                # Skip empty cells
                 if val == '.':
                     continue
-                    
-                # Calculate sub-box coordinate identifier (0-2, 0-2)
                 box_key = (i // 3, j // 3)
-                
-                # Check for duplicates in row, column, or sub-box
                 if val in rows[i] or val in cols[j] or val in boxes[box_key]:
                     return False
-                
-                # Add value to the respective sets
                 rows[i].add(val)
                 cols[j].add(val)
                 boxes[box_key].add(val)
-                
         return True
