035292fae6ebb0dd2b3ec9a6905f4880e46cce2f
King <vaghasiyakunj42@gmail.com>
2026-07-26 20:33:12 +0530

    Time: 6 ms (71.38%), Space: 23.5 MB (26.83%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,13 +1,13 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        nums.sort()
-        c=1
-        r=set()
-        for i in range(0,len(nums)):
-            if nums[i-1]==nums[i]:
-                c+=1
+        f={}
+        r=[]
+        for x in nums:
+            if x in f:
+                f[x]+=1
             else:
-                c=1
-            if c>len(nums)/3:
-                r.add(nums[i])
-        return list(r)
+                f[x]=1
+        for key,val in f.items():
+            if val>len(nums)/3:
+                r.append(key)
+        return r
