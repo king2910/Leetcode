4f54a7c3ffa98fbdefd3d5e518b5731aea51075c
King <vaghasiyakunj42@gmail.com>
2026-08-01 22:50:40 +0530

    Time: 7 ms (23.12%), Space: 19.4 MB (61.09%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.py
+++ 1089-duplicate-zeros/1089-duplicate-zeros.py
@@ -3,8 +3,7 @@ class Solution:
         i=0
         while i<len(arr):
             if arr[i]==0:
-                arr.insert(i+1,0)
-                i+=2
-                arr.pop()
-            else:
+                arr.insert(i,0)
                 i+=1
+                arr.pop()
+            i+=1
