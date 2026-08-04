936ffbfa3ec6528c81c1b675ce4bf7ff229aff90
King <vaghasiyakunj42@gmail.com>
2026-08-04 20:58:04 +0530

    Time: 42 ms (75%), Space: 19.2 MB (43.43%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
@@ -1,3 +1,16 @@
 class Solution:
     def interpret(self, command: str) -> str:
-        return command.replace('()','o').replace('(al)',"al")        
+        r=''
+        c=0
+        for x in command:
+            if x=='G':
+                r+='G'
+            elif x=='(' or x=='a' or x=='l':
+                c+=1
+            else:
+                if c==1:
+                    r+='o'
+                else:
+                    r+='al'
+                c=0
+        return r
