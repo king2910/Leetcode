e516a5fa0fc7882103cada4d066f937e13846866
King <vaghasiyakunj42@gmail.com>
2026-08-05 19:50:58 +0530

    Time: 77 ms (32.66%), Space: 19.4 MB (57.22%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
@@ -3,9 +3,9 @@ class Solution:
         r=int(c**(0.5))
         l=0
         while l<=r:
-            if l**2 + r**2==c:
+            if l*l + r*r==c:
                 return True
-            elif l**2 + r**2<c:
+            elif l*l + r*r<c:
                 l+=1
             else:
                 r-=1
